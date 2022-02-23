efd72eb4a6d3dd6ab091b48739d2490721960212
Yanting Yang <yy3189@columbia.edu>
2022-02-23 23:03:15 +0000

    update contents

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+source .venv/bin/activate;
+export FLASK_APP=flaskr;
+export FLASK_RUN_PORT=5000;
+export FLASK_RUN_HOST=0.0.0.0;
+
+case "$1" in
+    "debug")
+        export FLASK_ENV=development;
+        flask run;;
+    "deploy")
+        export FLASK_ENV=production;
+        nohup flask run >./logs/$(date "+%Y-%m-%d").log 2>&1 &;;
+    "kill")
+        str=$(netstat -tunlp 2>/dev/null);
+        echo "$str"|grep ":5000";
+        pid=$(echo "$str"|grep ":5000"|grep -Po '[0-9]+(?=\/python)');
+        echo kill $pid;;
+    "init")
+        python -m venv .venv;
+        source .venv/bin/activate;
+        pip install --upgrade pip;
+        pip install -r requirements.txt;;
+esac
