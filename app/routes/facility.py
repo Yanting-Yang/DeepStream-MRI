7b4899dccb064543952cccba6f9e4133df3fdca1
Yanting Yang <yy3189@columbia.edu>
2022-03-27 18:37:18 -0400

    feat(facility): add contents

--- app/routes/facility.py
+++ app/routes/facility.py
@@ -4,3 +4,19 @@ from flask import render_template
 @Route.route('/facility')
 def Facility():
     return render_template('facility.html')
+
+@Route.route('/facility/wet-lab-equipments')
+def Wet_Lab_Equipments():
+    return render_template('wet-lab-equipments.html')
+
+@Route.route('/facility/computing-facilities')
+def Computing_Facilities():
+    return render_template('computing-facilities.html')
+
+@Route.route('/facility/bruker-biospec-94-30')
+def Bruker_Biospec_94_30():
+    return render_template('bruker-biospec-94-30.html')
+
+@Route.route('/facility/physiological-monitoring-devices')
+def Physiological_Monitoring_Devices():
+    return render_template('physiological-monitoring-devices.html')
