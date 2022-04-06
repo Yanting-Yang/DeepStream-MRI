a3510deab9c63940b9c8224f0ee48c670f184319
Yanting Yang <yy3189@columbia.edu>
2022-04-06 19:33:55 -0400

    feat: change team.html structure

--- app/root/team.py
+++ app/root/team.py
@@ -1,6 +1,11 @@
 from .blueprint import root
 from flask import render_template
+import pandas as pd
 
 @root.route('/team')
 def team():
-    return render_template('team.html')
+    path = 'app/static/members.csv'
+    df = pd.read_csv(path)
+    df.fillna('',inplace=True)
+    data = df.to_dict(orient='index')
+    return render_template('team.html', data=data)
