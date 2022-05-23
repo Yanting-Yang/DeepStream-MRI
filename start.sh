bbb7a1ee26a1c3729fc9ddeb056e4687d644181c
Yanting Yang <yy3189@columbia.edu>
2022-05-22 21:01:49 -0400

    Add github action

--- start.sh
+++ start.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 app="deepstream-mri"
 username="yangyanting"
-docker build -t ${username}/${app} .
-docker run -d -p 127.0.0.1:5000:5000 --name=${app} ${username}/${app}
+
+case "$1" in
+    "build_run")
+        docker build -t ${username}/${app} .
+        docker run -d -p 127.0.0.1:5000:5000 --name=${app} ${username}/${app}
+        ;;
+    "pull_run")
+        docker pull ${username}/${app}
+        docker stop ${app}
+        docker rm ${app}
+        docker run --init -d -p 127.0.0.1:5000:5000 --name=${app} ${username}/${app}
+        ;;
+esac
+
+
