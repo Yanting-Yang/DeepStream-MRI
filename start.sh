682f01c96bc03f65e9baddfc5f962c66f47a95dc
Yanting Yang <yy3189@columbia.edu>
2022-05-22 23:29:45 -0400

    Update flask version

--- start.sh
+++ start.sh
@@ -3,14 +3,18 @@ app="deepstream-mri"
 username="yangyanting"
 
 case "$1" in
-    "build_run")
-        docker build -t ${username}/${app} .
-        docker run -d -p 127.0.0.1:5000:5000 --name=${app} ${username}/${app}
+    "debug")
+        docker stop ${app}-debug
+        docker rm ${app}-debug
+        docker build -t ${app}:debug .
+        docker image prune -f
+        docker run -p 127.0.0.1:5000:5000 --name=${app}-debug ${app}:debug
         ;;
     "pull_run")
         docker pull ${username}/${app}
         docker stop ${app}
         docker rm ${app}
+        docker image prune -f
         docker run --init -d -p 127.0.0.1:5000:5000 --name=${app} ${username}/${app}
         ;;
 esac
